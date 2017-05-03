9b094e0230678f481c5cb8b30906e9a99ae95f0f
Daniel von der Haar <daniel.von.der.haar@uni-oldenburg.de>
2017-05-04 01:36:51 +0200

    Formatierung

--- server/src/User.java
+++ server/src/User.java
@@ -1,56 +1,57 @@
 public class User {
-    private int id;
-    private String username;
-    private String passwordHash;
-    private String email;
 
-    /**
-     * Erstellt ein neues User-Objekt
-     *
-     * @param	id			ID des Users
-     * @param	username 	Name des Users
-     * @param	password 	Gehashtes Passwort des Users
-     * @param	email		E-Mail des Users
-     */
-    public User(int id, String username, String passwordHash, String email) {
-        this.id = id;
-        this.username = username;
-        this.passwordHash = passwordHash;
-        this.email = email;
-    }
+  private int id;
+  private String username;
+  private String passwordHash;
+  private String email;
 
-    /*
-	 * Getter und Setter
-	 */
-    public int getId() {
-        return id;
-    }
+  /**
+   * Erstellt ein neues User-Objekt
+   *
+   * @param  id      ID des Users
+   * @param  username Name des Users
+   * @param  password Gehashtes Passwort des Users
+   * @param  email    E-Mail des Users
+   */
+  public User(int id, String username, String passwordHash, String email) {
+    this.id = id;
+    this.username = username;
+    this.passwordHash = passwordHash;
+    this.email = email;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  /*
+ * Getter und Setter
+ */
+  public int getId() {
+    return id;
+  }
 
-    public String getUsername() {
-        return username;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    public String getPasswordHash() {
-        return passwordHash;
-    }
+  public void setUsername(String username) {
+    this.username = username;
+  }
 
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
+  public String getPasswordHash() {
+    return passwordHash;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public void setPasswordHash(String passwordHash) {
+    this.passwordHash = passwordHash;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
 }
