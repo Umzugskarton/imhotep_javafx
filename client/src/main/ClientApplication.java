a4651f39ed38a0d864e3e370ce6666d707b248a3
Tobias <tobias.hoffmann@uni-oldenburg.de>
2017-05-16 23:49:21 +0200

    Client terminiert jetzt ordnungsgemäß wenn Fenster geschlossen wird

--- client/src/main/ClientApplication.java
+++ client/src/main/ClientApplication.java
@@ -2,15 +2,27 @@ package main;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import socket.ClientSocket;
 
-public class ClientApplication extends Application{
+public class ClientApplication extends Application {
     private Stage stage;
+    private SceneController sceneController;
+
+    // Socket, um bei Beenden des Clients Verbindung zu beenden
+    private ClientSocket clientSocket;
 
     public static void main(String[] args){
         launch();
     }
 
+    @Override
     public void start(Stage stage) {
-        SceneController sc = new SceneController(stage);
+        this.sceneController = new SceneController(stage);
+        this.clientSocket = sceneController.getClientSocket();
+    }
+
+    @Override
+    public void stop() {
+        this.clientSocket.close();
     }
 }
