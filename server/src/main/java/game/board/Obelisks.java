164540843316098b1ea614c7e427f7c5fe167471
mmaszuhn <matthias.maszuhn@uni-oldenburg.de>
2017-11-10 01:31:44 +0100

    Obelisk an "Stone"-Klasse angepasst. Noch keinen schönen Weg gefunden, die Punkte bei Gleichstand zu verteilen

--- server/src/main/java/game/board/Obelisks.java
+++ server/src/main/java/game/board/Obelisks.java
@@ -26,57 +26,54 @@ public class Obelisks extends Site
   // müssen wir später testen
   @Override
   public int[] getPoints() {
-    int[] points = new int[5];
+    int[] points = new int[4];
     Integer[] stonesPerPlayer = new Integer[4];
-    /*
-    stonesPerPlayer[0] = Collections.frequency(obelisks, Player.PLAYER1);
-    stonesPerPlayer[1] = Collections.frequency(obelisks, Player.PLAYER2);
-    stonesPerPlayer[2] = Collections.frequency(obelisks, Player.PLAYER3);
-    stonesPerPlayer[3] = Collections.frequency(obelisks, Player.PLAYER4);
-    */
+    for(Stone stone : obelisks){
+      stonesPerPlayer[stone.getPlayer().getId()]++;
+    }
     Integer[] players = new Integer[4];
     players[0] = stonesPerPlayer[0];
     players[1] = stonesPerPlayer[1];
     players[2] = stonesPerPlayer[2];
     players[3] = stonesPerPlayer[3];
     Arrays.sort(stonesPerPlayer, Collections.reverseOrder());
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < players.length; i++) {
       if (players[i] == 0) {
-        points[i + 1] = 0;
+        points[i] = 0;
         break;
       }
       switch (playerCount) {
         case 2:
           if (players[i] == stonesPerPlayer[0]) {
-            points[i + 1] = 10;
+            points[i] = 10;
           }
           if (players[i] == stonesPerPlayer[1]) {
-            points[i + 1] = 1;
+            points[i] = 1;
           }
           break;
         case 3:
           if (players[i] == stonesPerPlayer[0]) {
-            points[i + 1] = 12;
+            points[i] = 12;
           }
           if (players[i] == stonesPerPlayer[1]) {
-            points[i + 1] = 6;
+            points[i] = 6;
           }
           if (players[i] == stonesPerPlayer[2]) {
-            points[i + 1] = 1;
+            points[i] = 1;
           }
           break;
         case 4:
           if (players[i] == stonesPerPlayer[0]) {
-            points[i + 1] = 15;
+            points[i] = 15;
           }
           if (players[i] == stonesPerPlayer[1]) {
-            points[i + 1] = 10;
+            points[i] = 10;
           }
           if (players[i] == stonesPerPlayer[2]) {
-            points[i + 1] = 5;
+            points[i] = 5;
           }
           if (players[i] == stonesPerPlayer[3]) {
-            points[i + 1] = 1;
+            points[i] = 1;
           }
           break;
         default:
