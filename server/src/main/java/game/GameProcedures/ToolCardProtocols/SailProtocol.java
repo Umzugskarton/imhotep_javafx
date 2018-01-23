27ee823ef9ec9ce65f045251470b463fc5dd088d
Nils <nils.spoering@uni-oldenburg.de>
2018-01-23 14:41:02 +0100

    aquireMove im Sail Protocol ausgelagert um redundanten Code zu verringern

--- server/src/main/java/game/GameProcedures/ToolCardProtocols/SailProtocol.java
+++ server/src/main/java/game/GameProcedures/ToolCardProtocols/SailProtocol.java
@@ -9,41 +9,41 @@ import GameMoves.VoyageToStoneSiteMove;
 import game.Game;
 
 public class SailProtocol implements Protocol {
-  private Game game;
-  private int playerId;
+    private Game game;
+    private int playerId;
 
-  public SailProtocol(Game game , int playerId){
-    this.game=game;
-    this.playerId=playerId;
-  }
+    public SailProtocol(Game game, int playerId) {
+        this.game = game;
+        this.playerId = playerId;
+    }
 
-  public void exec(){
-    game.sendAll(new ToolCardEvent("Sail", playerId ,true));
+    public void exec() {
+        game.sendAll(new ToolCardEvent("Sail", playerId, true));
 
-
-    for (int i = 0; i < 2 ; i++){
-      if (i == 0) {
-      game.sendTo(game.getOrder()[playerId].getUser(), new LoadUpShipExclusiveEvent());
-      game.getExecutor().waitForMove();
-      if(game.getExecutor().getMove() != null) {
-        Move move = game.getExecutor().getMove();
-          if (move instanceof LoadUpShipMove){
-            game.executeMove(move);
-          }
+        Move move;
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
+                game.sendTo(game.getOrder()[playerId].getUser(), new LoadUpShipExclusiveEvent());
+                move = aquireMove();
+                if (move instanceof LoadUpShipMove) {
+                    game.executeMove(move);
+                }
+            } else {
+                game.sendTo(game.getOrder()[playerId].getUser(), new VoyageToStoneSiteExclusiveEvent());
+                move = aquireMove();
+                if (move instanceof VoyageToStoneSiteMove) {
+                    game.executeMove(move);
+                }
+            }
         }
-      }
-      else{
-        game.sendTo(game.getOrder()[playerId].getUser(), new VoyageToStoneSiteExclusiveEvent());
+    }
+
+    private Move aquireMove() {
         game.getExecutor().waitForMove();
-        if(game.getExecutor().getMove() != null) {
-          Move move = game.getExecutor().getMove();
-          if (move instanceof VoyageToStoneSiteMove){
-            game.executeMove(move);
-          }
-        }
-      }
-      }
-      }
+        if (game.getExecutor().getMove() != null)
+            return game.getExecutor().getMove();
+        return null;
+    }
 }
 
 
