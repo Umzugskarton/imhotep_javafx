0241f71e9070848bd832c3c463259c7e4e56aa53
Umzugskarton <fabian.rieger130@gmail.com>
2018-01-23 15:53:56 +0100

    Hammer Blauer marktkarte im Server realisiert

--- server/src/main/java/game/GameProcedures/ToolCardProtocols/HammerProtocol.java
+++ server/src/main/java/game/GameProcedures/ToolCardProtocols/HammerProtocol.java
@@ -1,17 +1,36 @@
 package game.GameProcedures.ToolCardProtocols;
 
+import GameEvents.ToolCardEvent;
+import GameEvents.VoyageToStoneSiteExclusiveEvent;
+import GameMoves.FillUpStorageMove;
+import GameMoves.Move;
+import GameMoves.VoyageToStoneSiteMove;
 import game.Game;
 
-public class HammerProtocol implements Protocol{
+public class HammerProtocol implements Protocol {
   private Game game;
   private int playerId;
 
-  public HammerProtocol(Game game , int playerId){
-    this.game=game;
-    this.playerId=playerId;
+  public HammerProtocol(Game game, int playerId) {
+    this.game = game;
+    this.playerId = playerId;
   }
 
-  public void exec(){
+  public void exec() {
+    game.sendAll(new ToolCardEvent("Hammer", playerId, true));
+    Move move;
+    game.executeMove(new FillUpStorageMove());
+    game.sendTo(game.getOrder()[playerId].getUser(), new VoyageToStoneSiteExclusiveEvent());
+    move = acquireMove();
+    if (move instanceof VoyageToStoneSiteMove) {
+      game.executeMove(move);
+    }
+  }
 
+  private Move acquireMove() {
+    game.getExecutor().waitForMove();
+    if (game.getExecutor().getMove() != null)
+      return game.getExecutor().getMove();
+    return null;
   }
 }
--- server/src/main/java/game/GameProcedures/ToolCardProtocols/Protocol.java
+++ server/src/main/java/game/GameProcedures/ToolCardProtocols/Protocol.java
@@ -1,7 +1,5 @@
 package game.GameProcedures.ToolCardProtocols;
 
-import GameEvents.ToolCardEvent;
-
 /**
  * Created by fabianrieger on 22.01.18.
  */
