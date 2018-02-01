446651ebf625c9d5415a6d9db8e1bfcf442eec5b
Dennis Lindt <dennis.lindt@uni-oldenburg.de>
2018-02-01 18:52:14 +0100

    LobbyTest-Methoden initiiert

---
+++ server/src/test/java/game/board/LobbyTest.java
@@ -0,0 +1,66 @@
+package game.board;
+
+import org.junit.Test;
+
+/**
+ * Created by Lindt on 01.02.2018.
+ */
+public class LobbyTest {
+
+
+    @Test
+    public void swapHostTest() {
+
+    }
+
+    @Test
+    public void getHostNameTest() {
+
+    }
+
+    @Test
+    public void joinPWTest() {
+
+    }
+
+    @Test
+    public void joinEventTest() {
+
+    }
+
+    @Test
+    public void getColorsTest(){
+
+    }
+
+    @Test
+    public void getUsersStringArrayTest() {
+
+    }
+
+    @Test
+    public void setReadyTest() {
+
+    }
+
+    @Test
+    public void replaceColorTest() {
+
+    }
+
+    @Test
+    public void getUserCountTest() {
+
+    }
+
+    @Test
+    public void generateColorsTest() {
+
+    }
+
+    @Test
+    public void leaveLobbyEventTest() {
+
+    }
+
+}
