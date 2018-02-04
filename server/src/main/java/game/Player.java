0e09df25c5d5c26c307b786989c5c8dc675ba7da
Daniel von der Haar <daniel.von.der.haar@uni-oldenburg.de>
2018-02-04 23:48:21 +0100

    - Inventory in Player verschoben

--- server/src/main/java/game/Player.java
+++ server/src/main/java/game/Player.java
@@ -3,6 +3,11 @@ package game;
 import data.user.User;
 import game.board.SupplySled;
 import game.board.cards.Card;
+import game.board.cards.OrnamentCard;
+import game.board.cards.StatueCard;
+import game.board.cards.ToolCard;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Repräsentiert einen Spieler. Enthält seine Punktanzahl, seine aktuellen Karten und seine Steine.
@@ -12,7 +17,7 @@ public class Player {
   private int id;
   private int points = 0;
   private User user;
-  private Inventory inventory;
+  private Inventory inventory = new Inventory();
   private SupplySled supplySled = new SupplySled();
 
   /**
@@ -24,7 +29,6 @@ public class Player {
   public Player(User user, int id) {
     this.user = user;
     this.id = id;
-    inventory = new Inventory();
   }
 
   public int getId() {
@@ -51,11 +55,60 @@ public class Player {
     return points;
   }
 
-  public Inventory getInventory() {
-    return inventory;
-  }
-
   public SupplySled getSupplySled() {
     return supplySled;
+  }
+
+  private class Inventory {
+
+    private ArrayList<OrnamentCard> ornamentCards = new ArrayList<>();
+    private ArrayList<ToolCard> toolCards = new ArrayList<>();
+    private ArrayList<StatueCard> statueCards = new ArrayList<>();
+
+    List<OrnamentCard> getOrnamentCards() {
+      return ornamentCards;
+    }
+
+    List<StatueCard> getStatueCards() {
+      return statueCards;
+    }
+
+    List<ToolCard> getToolCards() {
+      return toolCards;
+    }
+
+    boolean ownsCard(Card card) {
+      if (card instanceof ToolCard) {
+        for (ToolCard tool : toolCards) {
+          if (tool.getType() == card.getType()) {
+            return true;
+          }
+        }
+        return false;
+      } else {
+        ArrayList<? extends Card> temp = new ArrayList<>();
+        if (card instanceof OrnamentCard) {
+          temp = ornamentCards;
+        } else if (card instanceof StatueCard) {
+          temp = statueCards;
+        }
+        for (Card c : temp) {
+          if (c.getType() == card.getType()) {
+            return true;
+          }
+        }
+        return false;
+      }
+    }
+
+    void addCard(Card card) {
+      if (card instanceof OrnamentCard) {
+        ornamentCards.add((OrnamentCard) card);
+      } else if (card instanceof ToolCard) {
+        toolCards.add((ToolCard) card);
+      } else if (card instanceof StatueCard) {
+        statueCards.add((StatueCard) card);
+      }
+    }
   }
 }
